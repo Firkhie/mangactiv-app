7afc3fa24a675986587a6fe7da74ccd0f50bb51c
Firdig Alfalakhi <Alfalakhie@gmail.com>
2023-07-04 22:17:41 +0700

    feat: add register and login controller

--- server-side/controllers/index.js
+++ server-side/controllers/index.js
@@ -1,2 +1,44 @@
-class Controller {}
+const { axios } = require("axios")
+const { comparePass } = require("../helpers/bcrypt")
+const { signToken } = require("../helpers/jwt")
+const { User, Bookmark } = require("../models")
+const { OAuth2Client } = require('google-auth-library')
+
+class Controller {
+  static async register(req, res, next) {
+    try {
+      const { username, email, password } = req.body
+      if (!username) throw { name: 'UsernameRequired' }
+      if (!email) throw { name: 'EmailRequired' }
+      if (!password) throw { name: 'PasswordRequired' }
+
+      const createdUser = await User.create({ username, email, password })
+      res.status(201).json({ id: createdUser.id, username: createdUser.username })
+    } catch (err) {
+      next(err)
+    }
+  }
+  static async login(req, res, next) {
+    try {
+      const { email, password } = req.body
+      if (!email) throw { name: 'EmailRequired' }
+      if (!password) throw { name: 'PasswordRequired' }
+
+      const user = await User.findOne({ where: { email } })
+      if (user) {
+        let checkPassword = comparePass(password, user.password)
+        if (checkPassword) {
+          let access_token = signToken({ id: user.id })
+          res.status(200).json({ access_token })
+        } else {
+          throw { name: 'EmailPasswordInvalid' }
+        }
+      } else {
+        throw { name: 'EmailPasswordInvalid' }
+      }
+    } catch (err) {
+      next(err)
+    }
+  }
+}
 module.exports = Controller
--- server-side/routes/index.js
+++ server-side/routes/index.js
@@ -4,17 +4,17 @@ const router = require('express').Router()
 
 router.post('/register', Controller.register)
 router.post('/login', Controller.login)
-router.post('/google-login', Controller.googleLogin)
+// router.post('/google-login', Controller.googleLogin)
 
-router.get('/popularmangas', Controller.fetchMangas)
-router.get('/updatedmangas', Controller.fetchMangas)
-router.get('/allmangas/:pageId', Controller.fetchMangas)
-router.get('/mangadetail/:id', Controller.fetchMangaDetail)
-router.get('/readmanga/:chapterId/:pageId', Controller.fetchMangaPages)
+// router.get('/popularmangas', Controller.fetchMangas)
+// router.get('/updatedmangas', Controller.fetchMangas)
+// router.get('/allmangas/:pageId', Controller.fetchMangas)
+// router.get('/mangadetail/:id', Controller.fetchMangaDetail)
+// router.get('/readmanga/:chapterId/:pageId', Controller.fetchMangaPages)
 
-router.use(authentication)
-router.post('/mylibrary/:id', Controller.createBookmark)
-router.get('/mylibrary', Controller.fetchBookmarks)
-router.delete('/mylibrary/:id', Controller.deleteBookmarkById)
+// router.use(authentication)
+// router.post('/mylibrary/:id', Controller.createBookmark)
+// router.get('/mylibrary', Controller.fetchBookmarks)
+// router.delete('/mylibrary/:id', Controller.deleteBookmarkById)
 
 module.exports = router
